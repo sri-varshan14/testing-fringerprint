e869228a0e81ba2943a318f8a4cae17788d92486
srivarshan geetha <srivarshan.geetha@blackbxttle.com>
2024-08-23 13:50:27 +0530

    Changes to new display details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,31 @@
 <script>
     import { onMount } from "svelte";
 
-    let id = undefined;
-    let fpPromise;
+    let requestId = undefined;
+    let visitorId = undefined;
+    let score = undefined;
+    let response = undefined;
 
     onMount(async () => {
         try {
             const FingerprintJS = await import(
                 "https://fpjscdn.net/v3/o1P0y7FQS6K1woZ7TD71"
             );
-            fpPromise = FingerprintJS.load();
+            let fpPromise = FingerprintJS.load();
             const fp = await fpPromise;
             const result = await fp.get();
-            id = result.visitorId;
+
+            requestId = result.requestId;
+            visitorId = result.visitorId;
+            score = result.confidence.score;
+            response = result;
         } catch (error) {
             console.error(error);
         }
     });
 </script>
 
-<p>{id}</p>
+<p>VisitorID: {visitorId}</p>
+<p>RequestID: {requestId}</p>
+<p>Score: {score}</p>
+<p>API Response: {JSON.stringify(response, null, 4)}</p>
